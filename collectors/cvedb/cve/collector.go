794f5169938dee5a9c5cfb7a8a0e79892abc2bd6
gitworkflows <118260833+gitworkflows@users.noreply.github.com>
2023-12-12 08:27:55 +0600

    test init

---
+++ collectors/cvedb/cve/collector.go
@@ -0,0 +1,170 @@
+package cve
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"strings"
+
+	"github.com/khulnasoft-lab/k8s-db-collector/collectors/cvedb/utils"
+
+	"github.com/hashicorp/go-multierror"
+)
+
+const (
+	k8svulnDBURL = "https://kubernetes.io/docs/reference/issues-security/official-cve-feed/index.json"
+	mitreURL     = "https://cveawg.mitre.org/api/cve"
+	cveList      = "https://www.cve.org/"
+	semver       = "SEMVER"
+)
+
+func Collect() (*K8sVulnDB, error) {
+	response, err := http.Get(k8svulnDBURL)
+	if err != nil {
+		return nil, err
+	}
+	var db K8sCVE
+	if err = json.NewDecoder(response.Body).Decode(&db); err != nil {
+		return nil, err
+	}
+	return ParseVulnDBData(db)
+}
+
+const (
+	// excludeNonCoreComponentsCves exclude  cves with missing data or non k8s core components
+	excludeNonCoreComponentsCves = "CVE-2019-11255,CVE-2020-10749,CVE-2020-8554"
+)
+
+func ParseVulnDBData(db K8sCVE) (*K8sVulnDB, error) {
+	fullVulnerabilities := make([]*Vulnerability, 0)
+	for _, item := range db.Items {
+		if strings.Contains(excludeNonCoreComponentsCves, item.ID) {
+			continue
+		}
+		for _, cveID := range utils.GetMultiIDs(item.ID) {
+			vulnerability, err := parseMitreCve(item.ExternalURL, cveID)
+			if err != nil {
+				return nil, err
+			}
+			if cveMissingImpoertantData(vulnerability) {
+				continue
+			}
+			component := utils.GetComponentFromDescription(item.ContentText, vulnerability.Component)
+			fullVulnerabilities = append(fullVulnerabilities, &Vulnerability{
+				ID:          cveID,
+				CreatedAt:   item.DatePublished,
+				Component:   getComponentName(component, vulnerability),
+				Affected:    getAffectedEvents(vulnerability),
+				Summary:     item.Summary,
+				Description: vulnerability.Description,
+				Urls:        []string{item.URL, item.ExternalURL},
+				CvssV3:      vulnerability.CvssV3,
+				Severity:    vulnerability.Severity,
+			})
+		}
+	}
+	err := validateCvesData(fullVulnerabilities)
+	if err != nil {
+		return nil, err
+	}
+	return &K8sVulnDB{fullVulnerabilities}, nil
+}
+
+func getAffectedEvents(v *Vulnerability) []*Affected {
+	affected := make([]*Affected, 0)
+	for _, av := range v.AffectedVersions {
+		if len(av.Introduced) == 0 {
+			continue
+		}
+		if av.Introduced == "0.0.0" {
+			av.Introduced = "0"
+		}
+		events := make([]*Event, 0)
+		ranges := make([]*Range, 0)
+		if len(av.Introduced) > 0 {
+			events = append(events, &Event{Introduced: av.Introduced})
+		}
+		if len(av.Fixed) > 0 {
+			events = append(events, &Event{Fixed: av.Fixed})
+		}
+		if len(av.LastAffected) > 0 && len(av.Fixed) == 0 {
+			events = append(events, &Event{LastAffected: av.LastAffected})
+		}
+		if len(av.Introduced) > 0 && len(av.LastAffected) == 0 && len(av.Fixed) == 0 {
+			events = append(events, &Event{LastAffected: av.Introduced})
+		}
+		ranges = append(ranges, &Range{
+			RangeType: semver,
+			Events:    events,
+		})
+		affected = append(affected, &Affected{Ranges: ranges})
+	}
+	return affected
+}
+
+func getComponentName(k8sComponent string, mitreCve *Vulnerability) string {
+	if len(k8sComponent) == 0 {
+		k8sComponent = mitreCve.Component
+	}
+	if strings.ToLower(mitreCve.Component) != "kubernetes" {
+		k8sComponent = mitreCve.Component
+	}
+	return strings.ToLower(fmt.Sprintf("%s/%s", utils.UpstreamOrgByName(k8sComponent), utils.UpstreamRepoByName(k8sComponent)))
+}
+
+func validateCvesData(cves []*Vulnerability) error {
+	var result error
+	for _, cve := range cves {
+		if len(cve.ID) == 0 {
+			result = multierror.Append(result, fmt.Errorf("\nid is mssing on cve #%s", cve.ID))
+		}
+		if len(cve.CreatedAt) == 0 {
+			result = multierror.Append(result, fmt.Errorf("\nCreatedAt is mssing on cve #%s", cve.ID))
+		}
+		if len(cve.Summary) == 0 {
+			result = multierror.Append(result, fmt.Errorf("\nSummary is mssing on cve #%s", cve.ID))
+		}
+		if len(strings.TrimPrefix(cve.Component, utils.UpstreamOrgByName(cve.Component))) == 0 {
+			result = multierror.Append(result, fmt.Errorf("\nComponent is mssing on cve #%s", cve.ID))
+		}
+		if len(cve.Description) == 0 {
+			result = multierror.Append(result, fmt.Errorf("\nDescription is mssing on cve #%s", cve.ID))
+		}
+		if len(cve.Affected) == 0 {
+			result = multierror.Append(result, fmt.Errorf("\nAffected Version is missing on cve #%s", cve.ID))
+		}
+		if len(cve.Affected) > 0 {
+			for _, v := range cve.Affected {
+				for _, r := range v.Ranges {
+					for i := 1; i < len(r.Events); i++ {
+						if len(r.Events[i-1].Introduced) == 0 {
+							result = multierror.Append(result, fmt.Errorf("\nAffectedVersion Introduced is missing from cve #%s", cve.ID))
+						}
+						if len(r.Events[i].Fixed) == 0 && len(r.Events[i].LastAffected) == 0 {
+							result = multierror.Append(result, fmt.Errorf("\nAffectedVersion Fixed and LastAffected are missing from cve #%s", cve.ID))
+						}
+					}
+				}
+			}
+		}
+		if cve.CvssV3.Score == 0 {
+			result = multierror.Append(result, fmt.Errorf("\nVector is mssing on cve #%s", cve.ID))
+		}
+		if cve.CvssV3.Vector == "" {
+			result = multierror.Append(result, fmt.Errorf("\nVector is mssing on cve #%s", cve.ID))
+		}
+		if cve.Severity == "" {
+			result = multierror.Append(result, fmt.Errorf("\nSeverity is mssing on cve #%s", cve.ID))
+		}
+		if len(cve.Urls) == 0 {
+			result = multierror.Append(result, fmt.Errorf("\nUrls is mssing on cve #%s", cve.ID))
+		}
+	}
+	return result
+}
+
+func cveMissingImpoertantData(vulnerability *Vulnerability) bool {
+	return len(vulnerability.Component) == 0 ||
+		len(vulnerability.AffectedVersions) == 0 ||
+		len(vulnerability.CvssV3.Vector) == 0
+}
---
+++ collectors/cvedb/cve/collector_test.go
@@ -0,0 +1,26 @@
+package cve
+
+import (
+	"encoding/json"
+	"os"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func Test_ParseVulneDB(t *testing.T) {
+	b, err := os.ReadFile("./testdata/k8s-db.json")
+	assert.NoError(t, err)
+	var bi K8sCVE
+	err = json.Unmarshal(b, &bi)
+	assert.NoError(t, err)
+	kvd, err := ParseVulnDBData(bi)
+	assert.NoError(t, err)
+	err = validateCvesData(kvd.Cves)
+	assert.NoError(t, err)
+	gotVulnDB, err := json.Marshal(kvd.Cves)
+	assert.NoError(t, err)
+	wantVulnDB, err := os.ReadFile("./testdata/expected-vulndb.json")
+	assert.NoError(t, err)
+	assert.Equal(t, string(wantVulnDB), string(gotVulnDB))
+}
